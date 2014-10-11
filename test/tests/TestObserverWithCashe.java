c5ac52de888dadd07bd0a4a581cb8d93d9147173
GarryTrue <garrytrue@yandex.ru>
2014-10-11 09:39:25 +0300

    Add TestCase for ObserverWithCash

---
+++ test/tests/TestObserverWithCashe.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import Implementation.PrintObserver;
+import Implementation.ObserverWithCashe;
+import Implementation.SimpleObserver;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author garrytrue
+ */
+public class TestObserverWithCashe {
+
+    ObserverWithCashe mObserverWithCashe;
+
+    @Before
+    public void setUp() {
+        mObserverWithCashe = new ObserverWithCashe();
+    }
+
+    @Test
+    public void dataInNotifyAndUpdateMustBeEquals() {
+        PrintObserver mock = mock(PrintObserver.class);
+        mObserverWithCashe.addObserver(mock);
+        mObserverWithCashe.notifyObservers("Masha");
+        verify(mock).update("Masha");
+    }
+
+    @Test
+    public void notifyTwoObservers() {
+        SimpleObserver mSimpleObserver = mock(SimpleObserver.class);
+        SimpleObserver mSimpleObserver1 = mock(SimpleObserver.class);
+        mObserverWithCashe.addObserver(mSimpleObserver);
+        mObserverWithCashe.addObserver(mSimpleObserver1);
+        mObserverWithCashe.notifyObservers("Peter");
+        verify(mSimpleObserver).update("Peter");
+        verify(mSimpleObserver1).update("Peter");
+    }
+
+}
